14b71eea18db707c65095cf4f2c2de16456578e9
Allan Martinez <martinezallan123@gmail.com>
2023-01-27 17:33:24 -0800

    setting up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+
+/** @type {import('tailwindcss').Config} */
+
+// https://letmefail.com/nextjs/how-to-add-custom-fonts-to-nextjs-and-tailwind-css-application/
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "ylw":"#fcb72b",
+        "dark-navy":"#495567",
+        "dim-grey":"#939caa",
+        "light-grey":"#efecf4",
+        "snow":"#f2f5f9",
+        "light-ylw":"#fff4df",
+      },
+      fontFamily: {
+        "space-mono": ["Space Mono", ...defaultTheme.fontFamily.sans],
+        "lexend-deca": ["Lexend Deca", ...defaultTheme.fontFamily.sans],
+
+      },
+    },
+  },
+  plugins: [],
+}
