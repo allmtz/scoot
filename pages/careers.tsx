437eb1eecb91c1cc04c2de6b4b2a7b3c8aebd431
Allan Martinez <martinezallan123@gmail.com>
2023-01-30 16:07:11 -0800

    adding page templates, WIP

---
+++ pages/careers.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Careers() {
+  return (
+    <>
+      <div>
+        <p>hello careers</p>
+        <Link href={"/"}>Home</Link>
+      </div>
+    </>
+  );
+}
---
+++ pages/location.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Location() {
+  return (
+    <>
+      <div>
+        <p>hello location</p>
+        <Link href={"/"}>Home</Link>
+      </div>
+    </>
+  );
+}
