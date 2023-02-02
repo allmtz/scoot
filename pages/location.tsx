05d5dc29c731fd8ca6540ffbeeae4212aca80f95
Allan <martinezallan123@gmail.com>
2023-02-02 00:50:52 -0800

    location tablet

--- pages/location.tsx
+++ pages/location.tsx
@@ -3,23 +3,33 @@ import Image from "next/image";
 //  components
 import { Container } from "@/components/Container";
 import { Nav } from "@/components/Nav";
+import { PageTitleHero } from "@/components/PageTitleHero";
 import { LocationCard } from "@/components/LocationCard";
 import { ImageBlock } from "@/components/ImageBlock";
+import { Button } from "@/components/Button";
 import { Footer } from "@/components/Footer";
 
-import bgImage from "../public/assets/images/careers-location-hero-mobile.jpg";
-import worldMap from "../public/assets/images/world-map-mobile.png";
+import bgMobile from "../public/assets/images/about-hero-mobile.jpg";
+import bgTablet from "../public/assets/images/about-hero-tablet.jpg";
+import bgDesktop from "../public/assets/images/about-hero-desktop.jpg";
+
+import worldMapMobile from "../public/assets/images/world-map-mobile.png";
 
 export default function Location() {
   return (
     <Container>
       <Nav />
+      <PageTitleHero
+        pageName="Locations"
+        mobileSrc={bgMobile}
+        tabletSrc={bgTablet}
+        desktopSrc={bgDesktop}
+      />
       <div className="relative flex items-center justify-center ">
-        <Image src={bgImage} alt=""></Image>
-        <h1 className="absolute text-white text-3xl">Locations</h1>
+        <Image src={worldMapMobile} alt="" className="mt-10"></Image>
       </div>
-      <Image src={worldMap} alt="" className="mt-10" />
-      <ul className="flex flex-col gap-4 items-center mt-8">
+
+      <ul className="flex flex-col gap-4 items-center my-10">
         <li>
           <LocationCard location="New York" />
         </li>
@@ -40,6 +50,21 @@ export default function Location() {
   requests and plan launches based on demand. Feel free to message us by clicking 
   the link or messaging us on social."
       />
+
+      {/* possibly use for desktop  */}
+      {/* <div className="flex flex-col items-center justify-center gap-8 md:flex-row md:p-4">
+        <h3 className="font-space-mono text-2xl text-center text-dark-navy md:text-lg">
+          <p>Your City Not Listed?</p>
+        </h3>
+        <p className="text-dim-grey text-center max-w-xl md:text-xs md:text-start md:w-[600px]">
+          If you’d like to see Scoot in your hometown, be sure to let us know.
+          We track requests and plan launches based on demand. Feel free to
+          message us by clicking the link or messaging us on social.
+        </p>
+        <button className="font-space-mono bg-ylw px-10 py-2 text-white ">
+          Message Us
+        </button>
+      </div> */}
 
       <Footer />
     </Container>
